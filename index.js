3714c5ae5fdf7ff936c059a1803d2553d2e00ff0
imxande <alerodriguezlarosa@gmail.com>
2020-05-12 17:54:25 -0400

    created an add vertex method

--- index.js
+++ index.js
@@ -3,7 +3,19 @@ class Graph {
         this.numberOfNodes = 0;
         this.adjacentList = {};
     }
+    addVertex(node){
+        this.adjacentList[node] = [];
+        this.numberOfNodes++;
+    }
 }
 
 const myGraph = new Graph();
-console.log(myGraph);  
+console.log(myGraph);
+myGraph.addVertex("0");  
+myGraph.addVertex("1");  
+myGraph.addVertex("2");  
+myGraph.addVertex("3");  
+myGraph.addVertex("4");  
+myGraph.addVertex("5");  
+myGraph.addVertex("6");
+console.log(myGraph);
