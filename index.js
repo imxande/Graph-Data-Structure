d85b047dd671f0d2e561907867b3a96664ee6b2c
imxande <alerodriguezlarosa@gmail.com>
2020-05-12 17:58:47 -0400

    created addEdge method

--- index.js
+++ index.js
@@ -6,7 +6,13 @@ class Graph {
     addVertex(node){
         this.adjacentList[node] = [];
         this.numberOfNodes++;
-    }
+    };
+
+    addEdge(node1, node2){
+        // undirected graph
+        this.adjacentList[node1].push(node2);
+        this.adjacentList[node2].push(node1);
+    };
 }
 
 const myGraph = new Graph();
@@ -19,3 +25,13 @@ myGraph.addVertex("4");
 myGraph.addVertex("5");  
 myGraph.addVertex("6");
 console.log(myGraph);
+myGraph.addEdge('3', '1'); 
+myGraph.addEdge('3', '4'); 
+myGraph.addEdge('4', '2'); 
+myGraph.addEdge('4', '5'); 
+myGraph.addEdge('1', '2'); 
+myGraph.addEdge('1', '0'); 
+myGraph.addEdge('0', '2'); 
+myGraph.addEdge('6', '5');
+console.log(myGraph);
+
